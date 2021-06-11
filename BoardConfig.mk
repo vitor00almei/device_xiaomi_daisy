85e995cbacfef0c833860a2e7d5a20a50756e9c2
vitor00almei <pondebatata@outlook.com>
2021-06-11 00:32:13 -0300

    daisy: import BoardConfig and bluetooth config file

---
+++ BoardConfig.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/daisy
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Filesystem
+TARGET_USES_MKE2FS := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := system/product
+TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
+
+# Kernel
+TARGET_KERNEL_CONFIG := sakura_defconfig
+
+# Partitions
+BOARD_HAS_REMOVABLE_STORAGE := true
+BOARD_VENDORIMAGE_PARTITION_SIZE := 805306368
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+
+# Recovery
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+# Vndk
+BOARD_VNDK_RUNTIME_DISABLE := true
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/daisy/BoardConfigVendor.mk
