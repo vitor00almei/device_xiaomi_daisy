d1541f6e031de40bfdfed7ab32aea15037369960
vitor00almei <pondebatata@outlook.com>
2021-06-11 00:48:47 -0300

    daisy: import pa overlays
    * for enable dt2w gesture

--- device.mk
+++ device.mk
@@ -52,6 +52,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-pa
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.daisy.rc
